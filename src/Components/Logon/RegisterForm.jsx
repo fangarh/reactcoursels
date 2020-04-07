985049be14257814fc56ccac87794ea8fb410df1
Dmitry Dvornikov <dvornikov.d@gmail.com>
2020-04-07 20:55:50 +0300

    Changed reg form

--- src/Components/Logon/RegisterForm.jsx
+++ src/Components/Logon/RegisterForm.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import NavigationActions from "../NavigationActions";
 import "./Logon.css";
 import PropTypes from "prop-types";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
 import { Logo } from "loft-taxi-mui-theme";
 
 const divInline = {
@@ -54,62 +56,56 @@ class RegisterForm extends React.Component {
                 Вход!
               </label>
             </div>
-            <div className="LogonInputBlock LogonInput-underline">
-              <input
+            <div className="LogonInputBlock ">
+              <Input
                 id="name"
                 name="user"
                 value={user}
                 type="text"
                 onChange={this.inputChangedEventHendler}
-                className="simpleLogonInput LogonInput "
+                className="simpleLogonInput  "
                 placeholder="email"
               />
             </div>
             <div style={divInline}>
-              <div
-                className="LogonInputBlock LogonInput-underline"
-                style={divInline}
-              >
-                <input
+              <div className="LogonInputBlock " style={divInline}>
+                <Input
                   id="firstname"
                   name="firstname"
                   value={firstname}
                   type="text"
                   onChange={this.inputChangedEventHendler}
-                  className="simpleLogonInput LogonInput "
+                  className="simpleLogonInput  "
                   placeholder="Имя"
                 />
               </div>
-              <div
-                className="LogonInputBlock LogonInput-underline"
-                style={divInline}
-              >
-                <input
+              <div className="LogonInputBlock " style={divInline}>
+                <Input
                   id="lastname"
                   name="lastname"
                   value={lastname}
                   type="text"
                   onChange={this.inputChangedEventHendler}
-                  className="simpleLogonInput LogonInput "
+                  className="simpleLogonInput  "
                   placeholder="Фамилия"
                 />
               </div>
             </div>
-            <div className="LogonInputBlock  LogonInput-underline">
-              <input
+            <div className="LogonInputBlock  ">
+              <Input
                 id="Password"
                 name="password"
                 value={password}
                 onChange={this.inputChangedEventHendler}
                 type="password"
-                className="simpleLogonInput LogonInput "
+                className="simpleLogonInput  "
                 placeholder="Пароль"
               />
             </div>
-            <div>
-              <button type="submit" className="SubmitDiv">
+            <div className="SubmitDiv">
+              <Button type="submit" variant="contained" color="primary">
                 Регистрация
-              </button>
+              </Button>
             </div>
           </div>
         </form>
